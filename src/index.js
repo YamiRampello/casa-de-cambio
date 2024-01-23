153b1a10c608f10df9e407ce6c283fdb91f39990
Yamila Rampello <99457830+YamiRampello@users.noreply.github.com>
2024-01-23 10:49:59 -0300

    Se crea interfaz para la API
    Se agrega la lógica de la interfaz utilizando Jquery. Para los estilos se utiliza css y bootstrap

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-console.log('funciona el index js')
+/// <reference types="jquery" />
+
+const URL = 'https://api.frankfurter.app';
+
+const $opcionesMoneda = document.querySelector('#ver-monedas');
+
+$opcionesMoneda.onclick = function () {
+  limpiarLista();
+
+  fetch(`${URL}/currencies`)
+    .then((respuesta) => respuesta.json())
+    .then((respuestaJSON) => {
+      Object.keys(respuestaJSON).forEach((moneda) => {
+        $('#lista-monedas').append(
+          $(`<li>${moneda}: ${respuestaJSON[moneda]}</li>`)
+        );
+      });
+    });
+};
+
+const $buscarDatos = document.querySelector('#buscar-datos');
+
+$buscarDatos.onclick = function () {
+  const $fecha = document.querySelector('#fecha').value;
+  const $moneda = document.querySelector('#moneda-base').value;
+
+  limpiarLista();
+
+  fetch(`${URL}/${$fecha}?from=${$moneda}`)
+    .then((respuesta) => respuesta.json())
+    .then((respuestaJSON) => {
+      $('#datos-solicitados').text(
+        `Cambios del día ${respuestaJSON.date} en base ${respuestaJSON.base}`
+      );
+      Object.keys(respuestaJSON.rates).forEach((moneda) => {
+        $('#lista-datos').append(
+          $(`<li>${moneda}: ${respuestaJSON.rates[moneda]}</li>`)
+        );
+      });
+    });
+};
+
+function limpiarLista() {
+  $('li').remove();
+}
