5072749193c11683054ed15aa14b4d476e407185
Yamila Rampello <99457830+YamiRampello@users.noreply.github.com>
2024-01-25 18:01:44 -0300

    Modifica ruta para que no se vea la IP
    En función de las correcciones recibidas, se establece una ruta base que se configura como una variable. Así, no queda escrita en la prueba pero cypress sabe donde encontrar la página

--- cypress/e2e/pruebas-interfaz.cy.js
+++ cypress/e2e/pruebas-interfaz.cy.js
@@ -1,8 +1,6 @@
-const URL = 'http://192.168.0.25:8080/';
-
 context('Interfaz para la API casa de cambio', () => {
   before(() => {
-    cy.visit(URL);
+    cy.visit('/');
   });
 
   describe('Prueba la interfaz', () => {
@@ -15,28 +13,28 @@ context('Interfaz para la API casa de cambio', () => {
     });
 
     it('se asegura que los inputs esten visibles', () => {
-      cy.visit(URL);
+      cy.visit('/');
       cy.get('.container').find('input').should('not.have.class', 'disabled');
     });
 
     it('se asegura que las opciones de monedas no esten visibles', () => {
-      cy.visit(URL);
+      cy.visit('/');
       cy.get('#lista-monedas').should('not.be.visible');
     });
 
     it('se asegura que los tipos de cambio no esten visibles', () => {
-      cy.visit(URL);
+      cy.visit('/');
       cy.get('#lista-moneda-fecha-solicitado').should('not.be.visible');
     });
 
     it('se asegura que al hacer click al boton muestre monedas', () => {
-      cy.visit(URL);
+      cy.visit('/');
       cy.get('#ver-monedas').click();
       cy.get('#lista-monedas').find('li').should('be.visible');
     });
 
     it('se asegura que al hacer click al boton muestre los cambios', () => {
-      cy.visit(URL);
+      cy.visit('/');
       cy.get('#buscar-tipos-de-cambio').click();
       cy.get('#lista-moneda-fecha-solicitado').find('li').should('be.visible');
     });
